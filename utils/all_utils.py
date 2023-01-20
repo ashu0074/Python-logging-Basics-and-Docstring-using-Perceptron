febf085f5b0c7190cf5d62ad87efc6d486080f16
ashishbhati <ashishbhati.1999@gmail.com>
2023-01-20 12:04:10 +0530

    docstring is added

--- utils/all_utils.py
+++ utils/all_utils.py
@@ -7,6 +7,15 @@ import logging
 
 # 1) now segrigate this so we can pass to the function
 def prepare_data(df, target_col = "y"):
+    """it is used to prepare the data and return the label and independent feature
+
+    Args:
+        df (pd.DataFrame): This is the Dataframe
+        target_col (str, optional): label col name. Defaults to "y".
+
+    Returns:
+        tuple: label and x
+    """
     logging.info("Preparing the data for training")
     X = df.drop(target_col , axis = 1)
     y = df[target_col]
